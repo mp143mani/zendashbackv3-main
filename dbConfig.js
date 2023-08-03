8aafdcb69a0d2430f630e0114b218214b78b8699
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-08-03 14:17:04 +0530

    s

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb')
 const dbName = 'ZenStdDashboard'
-const dbUrl = `mongodb+srv://Shubham:Shubham@shubham.d1qasaw.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbUrl = `mongodb+srv://mani143tech:mani4213@cluster0.ckxmigv.mongodb.net/${dbName}?retryWrites=true&w=majority`
 const MongoClient=mongodb.MongoClient
 module.exports ={mongodb,dbName,dbUrl,MongoClient}
